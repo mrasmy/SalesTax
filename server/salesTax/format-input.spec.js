8019a7df13932e52e6010fdc2dc3f7fa1a88b773
mrasmy <mrasmyf@eg.ibm.com>
2017-06-06 14:19:43 +0200

    updates in test file

--- server/salesTax/format-input.spec.js
+++ server/salesTax/format-input.spec.js
@@ -1,4 +1,4 @@
-import {formatInput,formatGroupItems,item,formatSpaceItem,formatPipeItem,formatCommaItem,splitNameFromQuantity} from './format-input';
+import {formatInput, formatGroupItems, item, formatSpaceItem, formatPipeItem, formatCommaItem, splitNameFromQuantity} from './format-input';
 
 describe.only('Test Format Input', () =>{
   let items;
@@ -10,88 +10,88 @@ describe.only('Test Format Input', () =>{
     true.should.be.true();
   });
 
-  it('Test formatInput',()=>{
-    let itemsBeforeFormatting = {"items":["comma:\n1 bottle of perfume, 27.99, imported\n","pipe:\nImported | 1 bottle of perfume | 47.50\n","space:\n1 book 12.49\n"]};
-    let commaItemObject = new item('bottle of perfume',' 27.99',' imported','1');
-    let pipeItemObject = new item('bottle of perfume ',' 47.50','Imported ','1');
-    let spaceItemObject = new item('book','12.49',null,'1');
-    let itemsAfterFormatting = {"itemsGroup":[{"items":[commaItemObject],"type":'comma'},{"items":[pipeItemObject],"type":'pipe'},{"items":[spaceItemObject],"type":'space'}]};
+  it('Test formatInput', ()=>{
+    let itemsBeforeFormatting = {'items': ['comma:\n1 bottle of perfume, 27.99, imported\n', 'pipe:\nImported | 1 bottle of perfume | 47.50\n', 'space:\n1 book 12.49\n']};
+    let commaItemObject = new item('bottle of perfume', ' 27.99', ' imported', '1');
+    let pipeItemObject = new item('bottle of perfume ', ' 47.50', 'Imported ', '1');
+    let spaceItemObject = new item('book', '12.49', null, '1');
+    let itemsAfterFormatting = {'itemsGroup': [{'items': [commaItemObject], 'type': 'comma'}, {'items': [pipeItemObject], 'type': 'pipe'}, {'items': [spaceItemObject], 'type': 'space'}]};
     formatInput(itemsBeforeFormatting).should.be.deepEqual(itemsAfterFormatting);
   });
 
-  it('Invalid Input Type',()=>{
-    let itemsBeforeFormatting = {"items":["nocomma:\n1 bottle of perfume, 27.99, imported\n","pipe:\nImported | 1 bottle of perfume | 47.50\n","space:\n1 book 12.49\n"]};
-    (function(){formatInput(itemsBeforeFormatting)}).should.throw('Invalid Type');
+  it('Invalid Input Type', ()=>{
+    let itemsBeforeFormatting = {'items': ['nocomma:\n1 bottle of perfume, 27.99, imported\n', 'pipe:\nImported | 1 bottle of perfume | 47.50\n', 'space:\n1 book 12.49\n']};
+    (function(){formatInput(itemsBeforeFormatting);}).should.throw('Invalid Type');
   });
 
-  it('No items passed to formatInput',()=>{
-    let itemsBeforeFormatting = {"items":[]};
-    (function(){formatInput(itemsBeforeFormatting)}).should.throw('No Group Items');
+  it('No items passed to formatInput', ()=>{
+    let itemsBeforeFormatting = {'items': []};
+    (function(){formatInput(itemsBeforeFormatting);}).should.throw('No Group Items');
   });
 
   it('Test formatGroupItems', () => {
-    let spaceItem = new item('book','12.49',null,'1');
-    let spaceitems = ['space:','1 book 12.49',''];
-    let result ={'type': 'space','items':[spaceItem]};
-    formatGroupItems(spaceitems,'space').should.be.deepEqual(result);
+    let spaceItem = new item('book', '12.49', null, '1');
+    let spaceitems = ['space:', '1 book 12.49', ''];
+    let result = {'type': 'space', 'items': [spaceItem]};
+    formatGroupItems(spaceitems, 'space').should.be.deepEqual(result);
   });
 
   it('Pass Empty list to formatGroupItems', () => {
-    (function(){formatGroupItems([],'space');}).should.throw('No Items');
+    (function(){formatGroupItems([], 'space');}).should.throw('No Items');
   });
 
   it('Pass Empty Type to formatGroupItems', () => {
-    let spaceItem = new item('book','12.49',null,'1');
-    let spaceitems = ['space:','1 book 12.49',''];
-    let result ={'type': 'space','items':[spaceItem]};
-    (function(){formatGroupItems(spaceitems,'');}).should.throw('Empty Type');
+    let spaceItem = new item('book', '12.49', null, '1');
+    let spaceitems = ['space:', '1 book 12.49', ''];
+    let result = {'type': 'space', 'items': [spaceItem]};
+    (function(){formatGroupItems(spaceitems, '');}).should.throw('Empty Type');
   });
 
   it('Pass a Type out of scope to formatGroupItems', () => {
-    let spaceItem = new item('book','12.49',null,'1');
-    let spaceitems = ['space:','1 book 12.49',''];
-    let result ={'type': 'space','items':[spaceItem]};
-    (function(){formatGroupItems(spaceitems,'spacett');}).should.throw('Type does not exist');
+    let spaceItem = new item('book', '12.49', null, '1');
+    let spaceitems = ['space:', '1 book 12.49', ''];
+    let result = {'type': 'space', 'items': [spaceItem]};
+    (function(){formatGroupItems(spaceitems, 'spacett');}).should.throw('Type does not exist');
   });
 
   it('Test formatSpaceItem', () => {
     let spaceItem = '1 book 12.49';
-    let spaceItemObject = new item('book','12.49',null,'1');
+    let spaceItemObject = new item('book', '12.49', null, '1');
     formatSpaceItem(spaceItem).should.be.deepEqual(spaceItemObject);
   });
 
   it('Pass Empty value to formatSpaceItem', () => {
-    (function(){formatSpaceItem('')}).should.be.throw('Item is empty');
+    (function(){formatSpaceItem('');}).should.be.throw('Item is empty');
   });
 
   it('Test formatCommaItem', () => {
     let commaItem = '1 bottle of perfume,27.99, imported';
-    let commaItemObject = new item('bottle of perfume','27.99',' imported','1');
+    let commaItemObject = new item('bottle of perfume', '27.99', ' imported', '1');
     formatCommaItem(commaItem).should.be.deepEqual(commaItemObject);
   });
 
   it('Pass Empty value to formatCommaItem', () => {
-    (function(){formatCommaItem('')}).should.be.throw('Item is empty');
+    (function(){formatCommaItem('');}).should.be.throw('Item is empty');
   });
 
-  it('Test formatPipeItem',() =>{
+  it('Test formatPipeItem', () =>{
     let pipeItem = 'Imported | 1 bottle of perfume | 47.50';
-    let pipeItemObject = new item('bottle of perfume ',' 47.50','Imported ','1');
+    let pipeItemObject = new item('bottle of perfume ', ' 47.50', 'Imported ', '1');
     formatPipeItem(pipeItem).should.be.deepEqual(pipeItemObject);
   });
 
   it('Pass Empty value to formatPipeItem', () => {
-    (function(){formatPipeItem('')}).should.be.throw('Item is empty');
+    (function(){formatPipeItem('');}).should.be.throw('Item is empty');
   });
 
-  it('Test splitNameFromQuantity',() =>{
+  it('Test splitNameFromQuantity', () =>{
     let itemQuantityName = '1 book';
-    let result = {'itemName':'book','quantity':'1'};
+    let result = {'itemName': 'book', 'quantity': '1'};
     splitNameFromQuantity(itemQuantityName).should.be.deepEqual(result);
   });
 
   it('Pass Empty value to splitNameFromQuantity', () => {
-    (function(){formatPipeItem('')}).should.be.throw('Item is empty');
+    (function(){formatPipeItem('');}).should.be.throw('Item is empty');
   });
 
 });
